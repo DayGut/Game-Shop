446b53b0cdd5cb5cf52f89dfcb66bdca3f12cc74
ulises <ulisesrcm001@gmail.com>
2022-03-05 21:57:50 -0300

    header

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 
 app.get( '/', (req, res) =>{
-    res.sendFile(path.join( __dirname, '/views/index.html'))
+    res.sendFile(path.join( __dirname, '/views/home.html'))
 })
 
 app.get( '/productDetail', (req, res) =>{
