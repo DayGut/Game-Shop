56401d7ebf58bd0db786a1c43d70c96a85034527
ulisesRCM <ulisesrcm001@gmail.com>
2022-04-19 17:18:46 -0300

    ordenar carpetas

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,12 +2,12 @@ const {getUsers, writeUsers} = require('../data');
 
 module.exports = {
     logueo: (req, res) => {
-        res.render('login', {
+        res.render('./login_register/login', {
             titulo: "Login"
         })
     }, 
     registro: (req, res) => {
-        res.render('register', {
+        res.render('./login_register/register', {
             titulo: "Registro"
         })
     }, 
