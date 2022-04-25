775bf2b6ff4c2bb04b92197abf6f41420806110f
ulisesRCM <ulisesrcm001@gmail.com>
2022-04-25 20:50:39 -0300

    function delet y errores

--- src/data/index.js
+++ src/data/index.js
@@ -5,8 +5,12 @@ module.exports = {
     products: JSON.parse(fs.readFileSync(path.join(__dirname, "/products.json"), "utf-8")),
     writeProducts: (data) => {
         fs.writeFileSync(path.join(__dirname, "/products.json"), JSON.stringify(data));
-     
 
     },
+    users: JSON.parse(fs.readFileSync(path.join(__dirname, "/user.json"), "utf-8")),
+    writeUsers: (data) => {
+        fs.writeFileSync(path.join(__dirname, "/user.json"), JSON.stringify(data));
+    },
+
     
 }
