11cf4e9fd6691a434f8caa368fc7b1f0cbd31658
ulisesrcm <ulisesrcm001@gmail.com>
2022-05-09 02:08:24 +0000

    register controler

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,11 +11,11 @@ module.exports = {
     },  
     processlogin: (req, res) =>{
         let user = users.find(user => user.email === req.body.email);
-
-        req.session.user = {
-            //id: user.id, 
-            usuario: user.email,
-            contraseña: user.password,
+        console.log(user);
+        req.user = {
+            id: user.id, 
+            email: user.email,
+            password: user.pass,
             avatar: user.avatar,
             rol: user.rol,
         }
@@ -25,12 +25,13 @@ module.exports = {
        res.redirect('/')
     },
 
-    registro: (req, res) => 
+    registro: (req, res) => {
         res.render('user/register', {
             titulo: "Registro",
             css:"register.css",
             session: req.session
-        }),
+        })
+    },
    
         processRegister: (req, res) => {
                 //Registrar un usuario - Guardarlo en el JSON
@@ -49,7 +50,7 @@ module.exports = {
                     id: lastId + 1,
                     name: req.body.name,
                     email: req.body.email,
-                    password: req.body.password,
+                    password: req.body.pass,
                     avatar: req.file ? req.file.filename : "user-avatar.jpeg",
                     rol: "user"
                 }
@@ -64,7 +65,7 @@ module.exports = {
     
                 // Paso 4 - Devolver respuesta (redirección)
     
-                res.redirect('/usuarios/login')
+                res.redirect('/user/login')
     
            
         }
