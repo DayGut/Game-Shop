d61aad9010c584a18fa999cbf8d41e5b4fce0843
ulisesrcm <ulisesrcm001@gmail.com>
2022-05-19 19:58:04 +0000

    Search-user

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,4 +1,8 @@
 const { products } = require('../data')
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const removeAccents = (str) => {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
 
 module.exports= {
 
@@ -12,6 +16,24 @@ module.exports= {
             session: req.session
         })
         
+    },
+    search: (req, res) => {
+        let searchResult = [];
+         products.forEach(product => {
+            if(removeAccents(product.name).toLowerCase().includes(req.query.keywords.toLowerCase())){
+                searchResult.push(product)
+            }
+        });
+        res.render('results', {
+            titulo: "Search",
+            searchResult,
+            keyword: req.query.keywords,
+            toThousand,
+            session: req.session
+            
+        })
+        
     }
+    
     
 }
