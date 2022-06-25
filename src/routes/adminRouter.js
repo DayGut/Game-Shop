5475655133a4710fa202b80ed269cba7faccebd6
ulisesrcm <ulisesrcm001@gmail.com>
2022-06-25 19:08:11 -0300

    validaciones

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -3,17 +3,20 @@ const router = express.Router();
 const adminController = require('../controllers/admin/adminController')
 const adminProductController = require('../controllers/admin/adminProductController')
 const uploadFile = require('../middlewares/uploadProductImage');
-const userSessionCheck = require('../middlewares/userSessionCheck');
+const addValidator = require('../validations/addProductValidator');
+const editValidator = require('../validations/editProductValidator');
 const adminCheck = require('../middlewares/adminCheck')
+const userSessionCheck = require('../middlewares/userSessionCheck');
+
 
 /*session*/
 router.get('/',userSessionCheck, adminCheck, adminController.index);
 
 //crud
+router.post('/productos',uploadFile.single('image'),addValidator,adminProductController.productCreate)//agregar
 router.get('/productos/agregar',userSessionCheck, adminCheck, adminProductController.productAdd);
 router.get('/productos/listar',userSessionCheck, adminCheck, adminProductController.list);//muestra
-router.post('/productos',uploadFile.single('image'), adminProductController.productCreate)//agregar
-router.put('/productos/:id',uploadFile.single('image'), adminProductController.productoEditado)//edita
+router.put('/productos/:id',uploadFile.single('image'), editValidator,adminProductController.productoEditado)//edita
 router.get('/productos/editar/:id',userSessionCheck, adminCheck, adminProductController.editProduct)//vista por get 
 router.delete('/productos/eliminar/:id', adminProductController.productDelete)//elimina 
 
---
+++ src/validations/editProductValidator.js
@@ -0,0 +1,24 @@
+const {check, body} = require('express-validator');
+
+let validateEditProducts = [
+    check("name")
+        .notEmpty().withMessage("Ingrese un nombre").bail()
+        .isAlphanumeric().withMessage("Nombre no valido").bail()
+        .isLength({min:5}).withMessage("Debe tener al menos 4 caracteres"),
+    check("price")
+         .notEmpty().withMessage("Ingerese un precio").bail()
+         .isNumeric().withMessage("Ingrese un valor numerico"),
+    check("categorias_id")
+         .notEmpty().withMessage("selecciona una categoria"),
+    body("discount").custom(value =>{
+        if(value >=0 && value <= 100){
+            return true;
+        }
+        return false;
+    }).withMessage("El descuesto tiene que ser entre 0 y 100")
+    
+
+ 
+];
+
+module.exports = validateEditProducts;
