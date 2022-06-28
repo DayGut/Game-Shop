e89a25ca250a08054701fa271c85baf1fc2bfc5b
daiana <daianayanel8@gmail.com>
2022-06-27 21:24:55 -0300

    campos a validar

--- src/validations/addProductValidator.js
+++ src/validations/addProductValidator.js
@@ -5,12 +5,22 @@ let validateAddProducts = [
         .notEmpty().withMessage("Ingrese un nombre").bail()
         .isAlphanumeric().withMessage("Nombre no valido").bail()
         .isLength({min:5}).withMessage("Debe tener al menos 5 caracteres"),
+        check("description")
+        .notEmpty().withMessage("Ingrese una descripcion").bail()
+        .isLength({min:20}).withMessage("Debe tener al menos 20 caracteres"),
     check("price")
          .notEmpty().withMessage("Ingerese un precio").bail()
          .isNumeric().withMessage("Ingrese un valor numerico"),
     check("categorias_id")
          .notEmpty().withMessage("selecciona una categoria"),
-         
+    check("image")
+            .custom(value =>{
+                if(value.match(/\.(jpg|jpeg|png|gif)$/)){
+                    return true;
+                }
+                return false;
+            }).withMessage("jpg, jpeg, png, gif"),
+            
     body("discount").custom(value =>{
         if(value >=0 && value <= 100){
             return true;
